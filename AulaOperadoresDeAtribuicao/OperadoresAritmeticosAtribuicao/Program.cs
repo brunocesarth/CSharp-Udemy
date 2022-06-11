a3d159846d3d61986c2eb40e4a2586b741be3a4f
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-06-11 01:41:29 -0300

    Aula de C# - Operadores de atribuição

---
+++ AulaOperadoresDeAtribuicao/OperadoresAritmeticosAtribuicao/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OperadoresAritmeticosAtribuicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            a++;
+            Console.WriteLine(a);
+
+            int b = 10;
+            b--;
+            Console.WriteLine(b);
+
+            int c = 10;
+            int d = c++; // Primeiro joga o valor de a para b, e depois incrementa a.
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+
+            int e = 10;
+            int f = ++e; // Primeiro incrementa o valor da variável a, e depois ele atribui o resultado a b.
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+        }
+    }
+}
