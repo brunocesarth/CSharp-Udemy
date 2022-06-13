1739564a0e5c721e0db508f30675035ca414fdbe
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-06-13 01:32:23 -0300

    Alteração nos comentários

--- AulaConversaoImplicitaECasting/AulaConversaoImplicitaECasting/Program.cs
+++ AulaConversaoImplicitaECasting/AulaConversaoImplicitaECasting/Program.cs
@@ -15,20 +15,19 @@ namespace AulaConversaoImplicitaECasting
             float b;
 
             a = 5.1;
-            b = (float) a; //Conversão explicita de tipos
+            b = (float) a; //Conversão explicita de tipos(CASTING)
             Console.WriteLine(b);
 
             double c;
             int d;
-
             c = 5.1;
-            d = (int)c; //Conversão explicita de tipos
+            d = (int)c; //Conversão explicita de tipos(CASTING)
             Console.WriteLine(d);
 
             int e = 5;
             int f = 2;
 
-            double resultado = (double)e / f;
+            double resultado = (double)e / f; //Necessário CASTING para o resultado sair em double
             Console.WriteLine(resultado);
         }
     }
