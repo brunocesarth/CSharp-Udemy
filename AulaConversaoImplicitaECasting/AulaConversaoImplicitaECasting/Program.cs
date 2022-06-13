e7116cd02fcc710b3af57d7ba1b9b71df348ce01
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-06-13 01:04:31 -0300

    Aula de C#
    Conversão implícita e explícita.

---
+++ AulaConversaoImplicitaECasting/AulaConversaoImplicitaECasting/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AulaConversaoImplicitaECasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x = 4.5f;
+            double y = x; //Conversão implicita de tipos
+
+            Console.WriteLine(y);
+
+            double a;
+            float b;
+
+            a = 5.1;
+            b = (float) a; //Conversão explicita de tipos
+            Console.WriteLine(b);
+
+            double c;
+            int d;
+
+            c = 5.1;
+            d = (int)c; //Conversão explicita de tipos
+            Console.WriteLine(d);
+
+            int e = 5;
+            int f = 2;
+
+            double resultado = (double)e / f;
+            Console.WriteLine(resultado);
+        }
+    }
+} 
