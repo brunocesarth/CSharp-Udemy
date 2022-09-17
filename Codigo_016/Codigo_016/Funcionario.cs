7cec619cb506e09c51dea0b902a12d018b809cd3
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-17 16:02:16 -0300

    Codigo 016

---
+++ Codigo_016/Codigo_016/Funcionario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Codigo_016 {
+    class Funcionario {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido() {
+            double salarioLiquido = SalarioBruto - Imposto;
+            return salarioLiquido;
+        }
+
+        public void AumentarSalario(double porcentagem) {
+            SalarioBruto += (SalarioBruto * (porcentagem / 100.00));
+        }
+
+        public override string ToString() {
+            return Nome
+                + ", $"
+                + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
