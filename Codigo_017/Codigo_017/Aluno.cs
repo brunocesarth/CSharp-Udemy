adeb1a719ee04f4e66b21ae6522d4050496f5247
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-17 17:04:21 -0300

    Codigo 017

---
+++ Codigo_017/Codigo_017/Aluno.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Codigo_017 {
+    class Aluno {
+        public string Nome;
+        public double Nota1, Nota2, Nota3;
+
+        public double NotaFinal() {
+            return Nota1 + Nota2 + Nota3;
+        }
+        public string Status() {
+            if (NotaFinal() >= 60.00) {
+                return "APROVADO";
+            } else {
+                return "REPROVADO";
+            }
+        }
+
+        public string NotaFaltante() {
+            double NotaFaltante = 60.00 - NotaFinal();
+            return NotaFaltante.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Codigo_017/Codigo_017/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Codigo_017 {
+    class Program {
+        static void Main(string[] args) {
+
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno: ");
+            aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Nota Final = " + aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(aluno.Status());
+            
+            if (aluno.NotaFinal() < 60.00) {
+                Console.WriteLine("Faltaram " + aluno.NotaFaltante() + " Pontos");
+            }
+        }
+    }
+}
