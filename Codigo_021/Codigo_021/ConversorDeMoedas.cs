63e3467304a530a64a60a62c6ae8965e962bf84c
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-18 10:58:58 -0300

    Codigo 021
    Exercício de fixação

---
+++ Codigo_021/Codigo_021/ConversorDeMoedas.cs
@@ -0,0 +1,10 @@
+﻿namespace Codigo_021 {
+    class ConversorDeMoedas {
+        public static double iof = 6.0;
+
+        public static double Calcular(double cotacao, double valor) {
+            double total = cotacao * valor;
+            return total  + ((total  * iof) / 100.00);
+        }
+    }
+}
---
+++ Codigo_021/Codigo_021/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Codigo_021 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Qual é a cotação do dolár? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos doláres você vai comprar? ");
+            double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double valorTotal = ConversorDeMoedas.Calcular(cotacao, valor);
+
+            Console.WriteLine("Valor a ser pago em reais = " + valorTotal.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
