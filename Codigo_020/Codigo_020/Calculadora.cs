4f979b29fae68884f548cdb5f4036b10f9efd462
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-18 01:02:20 -0300

    Codigo 020
    Membros estaticos pt 2

---
+++ Codigo_020/Codigo_020/Calculadora.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Codigo_020 {
+    class Calculadora {
+        public static double Pi = 3.14;
+
+        public static double Circunferencia(double r) {
+            return 2.0 * Pi * r;
+        }
+
+        public static double Volume(double r) {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0);
+        }
+    }
+}
