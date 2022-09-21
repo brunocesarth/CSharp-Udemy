538aacbf995b1d6a8a7c3991b6a1cc48620c5cc3
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-21 09:58:00 -0300

    Codigo 027
    Properties

---
+++ Codigo_027/Codigo_027/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Codigo_027 {
+    class Program {
+        static void Main(string[] args) {
+            Produto p = new Produto("TV", 500.00, 10);
+
+            p.Nome = "T";
+            //p.Preco => Não funciona pq é somente leitura
+
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco);
+        }
+    }
+}
