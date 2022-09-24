ea1439729c1e8a382ae9609ce4cee49c36247276
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-24 15:21:43 -0300

    Codigo 030

---
+++ Codigo_030/Codigo_030/Point.cs
@@ -0,0 +1,9 @@
+﻿namespace Codigo_030 {
+    struct Point {
+        public double X;
+        public double Y;
+        public override string ToString() {
+            return "(" + X + ", " + Y + ")";
+        }
+    }
+}
---
+++ Codigo_030/Codigo_030/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Codigo_030 {
+    class Program {
+        static void Main(string[] args) {
+            Point p;
+            p.X = 10;
+            p.Y = 20;
+            Console.WriteLine(p);
+            p = new Point();
+            Console.WriteLine(p);
+        }
+    }
+}
