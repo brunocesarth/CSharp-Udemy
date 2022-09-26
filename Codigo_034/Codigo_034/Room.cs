d450998493ed93058587e1982bedb5fb009f3978
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-26 20:54:57 -0300

    Update Room.cs

--- Codigo_034/Codigo_034/Room.cs
+++ Codigo_034/Codigo_034/Room.cs
@@ -1,20 +1,14 @@
 ﻿namespace Codigo_034 {
     class Room {
-        public string Nome {
-            get; set;
-        }
-        public string Email {
-            get; set;
-        }
+        public string Nome { get; set; }
+        public string Email { get; set; }
         public Room(string nome, string email) {
             Nome = nome;
             Email = email;
         }
 
         public override string ToString() {
-            return Nome
-                + ", "
-                + Email;
+            return Nome + ", " + Email;
         }
     }
 }
