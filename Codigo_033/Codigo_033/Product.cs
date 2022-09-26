d5d15fa7a8ad58ad452b64004d4d01105a0ffcb4
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-26 09:48:40 -0300

    Codigo 033
    Vetores parte 2

---
+++ Codigo_033/Codigo_033/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Codigo_033 {
+    class Product {
+        public string Name {
+            get; set;
+        }
+        public double Price {
+            get; set;
+        }
+    }
+}
