f3697ede705032331ac3d52068fd42b1a0499391
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-26 11:19:17 -0300

    Codigo 034
    Exercicio fixação

---
+++ Codigo_034/Codigo_034/Room.cs
@@ -0,0 +1,20 @@
+﻿namespace Codigo_034 {
+    class Room {
+        public string Nome {
+            get; set;
+        }
+        public string Email {
+            get; set;
+        }
+        public Room(string nome, string email) {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString() {
+            return Nome
+                + ", "
+                + Email;
+        }
+    }
+}
