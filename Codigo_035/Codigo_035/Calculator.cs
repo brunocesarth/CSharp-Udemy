66f6b5fe801f04b4f074e6ceb096eca8fc0f7534
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-26 20:00:14 -0300

    Codigo 035
    Modificador de parametros Params

---
+++ Codigo_035/Codigo_035/Calculator.cs
@@ -0,0 +1,16 @@
+﻿namespace Codigo_035 {
+    class Calculator {
+
+        public static int Sum(int n1, int n2) {
+            return n1 + n2;
+        }
+
+        public static int Sum(int n1, int n2, int n3) {
+            return n1 + n2 + n3;
+        }
+
+        public static int Sum(int n1, int n2, int n3, int n4) {
+            return n1 + n2 + n3 + n4;
+        }
+    }
+}
