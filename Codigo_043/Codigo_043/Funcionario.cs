b27849c774c7cd2c230802f1dc288105b8b0e2ae
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-27 21:40:29 -0300

    Codigo 043
    Exercicio de fixação listas, em andamento

---
+++ Codigo_043/Codigo_043/Funcionario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codigo_043 {
+    class Funcionario {
+        public int Id { get; private set; }
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(int id, string nome, double salario) {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+    }
+}
---
+++ Codigo_043/Codigo_043/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Codigo_043 {
+    class Program {
+        static void Main(string[] args) {
+            List<Funcionario> listaDeFuncionarios = new List<Funcionario>();
+
+            Console.Write("Quantos funcionários serão registrados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine($"Funcionário: #{i}");
+                Console.Write("ID: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Salário: ");
+                double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                listaDeFuncionarios.Add(new Funcionario(id, nome, salario));
+                Console.WriteLine();
+            }
+        }
+    }
+}
