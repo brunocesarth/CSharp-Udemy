83fb2e4dc6d191a8e0aea87820fc7f4e3c6e79d4
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-28 11:17:12 -0300

    Alterações novas

--- Codigo_043/Codigo_043/Funcionario.cs
+++ Codigo_043/Codigo_043/Funcionario.cs
@@ -1,17 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Globalization;
 
 namespace Codigo_043 {
     class Funcionario {
         public int Id { get; private set; }
-        public string Nome { get; set; }
-        public double Salario { get; set; }
+        public string Nome { get; private set; }
+        public double Salario { get; private set; }
 
         public Funcionario(int id, string nome, double salario) {
             Id = id;
             Nome = nome;
             Salario = salario;
+        }
+
+        public Funcionario(int id) {
+            Id = id;
+        }
+
+        public void AumentaSalario(double porcentagem) {
+            Salario += Salario * porcentagem;
+        }
+
+        public override string ToString() {
+            return Id + ", " + Nome + ", " + Salario.ToString("F2", CultureInfo.InvariantCulture);
         }
     }
 }
--- Codigo_043/Codigo_043/Program.cs
+++ Codigo_043/Codigo_043/Program.cs
@@ -6,6 +6,8 @@ namespace Codigo_043 {
     class Program {
         static void Main(string[] args) {
             List<Funcionario> listaDeFuncionarios = new List<Funcionario>();
+            Funcionario funcionario;
+            int id;
 
             Console.Write("Quantos funcionários serão registrados? ");
             int n = int.Parse(Console.ReadLine());
@@ -13,13 +15,20 @@ namespace Codigo_043 {
             for (int i = 1; i <= n; i++) {
                 Console.WriteLine($"Funcionário: #{i}");
                 Console.Write("ID: ");
-                int id = int.Parse(Console.ReadLine());
+                id = int.Parse(Console.ReadLine());
                 Console.Write("Nome: ");
                 string nome = Console.ReadLine();
                 Console.Write("Salário: ");
                 double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                listaDeFuncionarios.Add(new Funcionario(id, nome, salario));
+                listaDeFuncionarios.Add(funcionario = new Funcionario(id, nome, salario));
                 Console.WriteLine();
+            }
+
+            Console.Write("Entre com o ID do funcionário que irá receber um aumento de salário: ");
+            id = int.Parse(Console.ReadLine());
+
+            foreach (Funcionario obj in listaDeFuncionarios) {
+                Console.WriteLine(obj);
             }
         }
     }
