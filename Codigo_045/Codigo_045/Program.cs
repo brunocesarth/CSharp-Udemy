9430548965a224391939e33e90f22840c2d99095
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-29 10:19:41 -0300

    Codigo_045
    Exercício resolvido

---
+++ Codigo_045/Codigo_045/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Codigo_045 {
+    class Program {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+            int[,] mat = new int[n, n];
+
+            for (int i = 0; i < n; i++) {
+                string[] s = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++) {
+                    mat[i, j] = int.Parse(s[j]);
+                }
+            }
+
+            Console.WriteLine("Main diagonal:");
+            for (int i = 0; i < n; i++) {
+                Console.WriteLine(mat[i,i] + " ");
+            }
+
+            Console.WriteLine();
+            int contaNegativos = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (mat[i,j] < 0) {
+                        contaNegativos++;
+                    }
+                }
+            }
+            Console.WriteLine("Negative numbers: " + contaNegativos);
+        }
+    }
+}
