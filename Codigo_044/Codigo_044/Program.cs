56a887df82d4b2272ad0494ce1962ef91f91fe97
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-29 09:34:11 -0300

    Codigo 044
    Matrizes

---
+++ Codigo_044/Codigo_044/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Codigo_044 {
+    class Program {
+        static void Main(string[] args) {
+            double[,] mat = new double[2, 3];
+
+            Console.WriteLine(mat.Length); //Fala quantos elementos tem na matriz
+            Console.WriteLine(mat.Rank); //Quanto é a primeira dimensão da matriz(linhas)
+            Console.WriteLine(mat.GetLength(0)); //Mostra o tamanho da matriz na primeira dimensão(linha)
+            Console.WriteLine(mat.GetLength(1)); //Mostra o tamanho da matriz na segunda dimensão(coluna)
+        }
+    }
+}
