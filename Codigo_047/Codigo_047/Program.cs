3cd46c875ebce98391d7a9043a24a77a8c146094
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-09-29 21:00:41 -0300

    Codigos 47/48/49

---
+++ Codigo_047/Codigo_047/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Codigo_047 {
+    class Program {
+        static void Main(string[] args) {
+            var x = 10;
+            var y = 20.0;
+            var z = "Maria";
+
+            var w = z;
+            //int a = z; Não deixa, pq são tipos diferentes
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+        }
+    }
+}
