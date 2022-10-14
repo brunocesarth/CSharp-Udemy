5d2a518177effc3c2ac7b5f546c3454a93df8c08
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-10-13 21:41:14 -0300

    Codigo 060
    Exercício resolvido primeira parte

---
+++ Codigo_060/Codigo_060/Entities/Department.cs
@@ -0,0 +1,12 @@
+﻿namespace Codigo_060.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+        public Department() { }
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Codigo_060/Codigo_060/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace Codigo_060.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
