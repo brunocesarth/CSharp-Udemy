19a88b752cd3fbdc6ec7d8e784b693fda3e44f58
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-10-14 21:33:24 -0300

    Codigo 062
    Exercício de fixação

---
+++ Codigo_062/Codigo_062/Entities/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Codigo_062.Entities;
+using Codigo_062.Entities.Enums;
+
+
+namespace Codigo_062.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+    }
+}
---
+++ Codigo_062/Codigo_062/Entities/OrderItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codigo_062.Entities
+{
+    class OrderItem
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+    }
+}
