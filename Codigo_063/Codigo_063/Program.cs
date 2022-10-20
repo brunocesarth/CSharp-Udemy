a0b35fe8697ce460370f32c323977b7ed781f93f
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-10-19 21:32:21 -0300

    Codigo 063

---
+++ Codigo_063/Codigo_063/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Codigo_063.Entities;
+
+
+namespace Codigo_063
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+            Console.WriteLine(account.Balance);
+
+            //account.Balance = 200.00;
+        }
+    }
+}
