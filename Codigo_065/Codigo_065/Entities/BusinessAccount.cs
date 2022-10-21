00d92249e5e79ef4284d603e2bc3ce0cd29d8f73
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-10-20 21:37:37 -0300

    Codigo 065

---
+++ Codigo_065/Codigo_065/Entities/BusinessAccount.cs
@@ -0,0 +1,20 @@
+﻿namespace Codigo_065.Entities
+{
+    class BusinessAccount : Account
+    {
+        public double LoanLimit { get; private set; }
+        public BusinessAccount() { }
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
---
+++ Codigo_065/Codigo_065/Entities/SavingsAccount.cs
@@ -0,0 +1,17 @@
+﻿namespace Codigo_065.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; private set; }
+        public SavingsAccount() { }
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
