f1461fa001855a4826cdc0079d313a696bf43299
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-10-21 10:43:08 -0300

    Codigo_065 - continuação

--- Codigo_065/Codigo_065/Entities/SavingsAccount.cs
+++ Codigo_065/Codigo_065/Entities/SavingsAccount.cs
@@ -13,5 +13,10 @@
         {
             Balance += Balance * InterestRate;
         }
+
+        public override void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
     }
 }
