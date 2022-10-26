93572634eb52552ecc20856393a3c02d7f1583e5
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-10-25 21:24:33 -0300

    Inicio codigo_074

---
+++ Codigo_074/Codigo_074/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Codigo_074
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Room number: ");
+            int roomNumber = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
