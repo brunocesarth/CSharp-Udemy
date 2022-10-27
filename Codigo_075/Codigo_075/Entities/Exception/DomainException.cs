f8711191d9c7c3274191157141bb45dd522692aa
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-10-27 10:15:01 -0300

    Codigo 075

---
+++ Codigo_075/Codigo_075/Entities/Exception/DomainException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Codigo_075.Entities.Exception
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message) { }
+    }
+}
