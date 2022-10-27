e8434f1894ebce5ed81ecbcc38fdd732f5413835
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-10-27 19:51:09 -0300

    Criando jogo de xadrez

---
+++ Xadrez/Xadrez/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using tabuleiro;
+
+namespace Xadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+            
+        }
+    }
+}
