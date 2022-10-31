ded9faf1488f68e68be8a804a9e40bbaf49aed81
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-10-31 10:12:14 -0300

    Update Program.cs

--- Codigo_081/Codigo_081/Program.cs
+++ Codigo_081/Codigo_081/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Codigo_081
 {
@@ -6,7 +7,24 @@ namespace Codigo_081
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"c:\temp\myfolder";
+
+            try
+            {
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES: ");
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+
+                Directory.CreateDirectory(path + @"\newfolder");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
