3a54d82f5098d73fed8dedd542c88b55abf461de
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-10-31 09:38:51 -0300

    Codigo 080

---
+++ Codigo_080/Codigo_080/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Codigo_080
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\myfolder";
+
+            try
+            {
+                //pode usar var no lugar de iEnumerable
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Folders:");
+                foreach(string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
