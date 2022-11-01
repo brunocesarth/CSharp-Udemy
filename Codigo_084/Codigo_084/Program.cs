f44f0e090b5bb9154b7ce783664b40ddbe7bbb06
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-11-01 11:47:14 -0300

    Código 084

--- Codigo_084/Codigo_084/Program.cs
+++ Codigo_084/Codigo_084/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using Codigo_084.Entities;
+using Codigo_084.Services;
 
 namespace Codigo_084
 {
@@ -6,8 +9,29 @@ namespace Codigo_084
     {
         static void Main(string[] args)
         {
-            
 
+            Console.WriteLine("Enter rental data");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+            Console.Write("Pickup (dd/mm/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Return (dd/mm/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Enter the price per hour: ");
+            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter the price per day: ");
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+            RentalService rentalService = new RentalService(hour, day);
+
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine();
+            Console.WriteLine("INVOICE:");
+            Console.WriteLine(carRental.Invoice);
         }
     }
 }
---
+++ Codigo_084/Codigo_084/Services/BrasilTaxService.cs
@@ -0,0 +1,17 @@
+﻿namespace Codigo_084.Services
+{
+    class BrasilTaxService
+    {
+        public double Tax(double amount)
+        {
+            if (amount <= 100.0)
+            {
+                return amount * 0.2;
+            }
+            else
+            {
+                return amount * 0.15;
+            }
+        }
+    }
+}
