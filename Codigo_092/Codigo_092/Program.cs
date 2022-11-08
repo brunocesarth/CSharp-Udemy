6298c1eda969ba0a7f83618107ccdbabce166657
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-11-07 21:27:17 -0300

    Código 092

---
+++ Codigo_092/Codigo_092/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Codigo_092
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService printService = new PrintService();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string x = Console.ReadLine();
+                printService.AddValue(x);
+            }
+
+            //Dá erro em tempo de execução, e não de compilação.
+            int a = (int)printService.First();
+            int b = a + 2;
+            Console.WriteLine(b);
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());
+        }
+    }
+}
