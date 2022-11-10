9e5cb0c70ff3526cbbb5d207fdb80183d26f548b
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-11-10 20:56:28 -0300

    Código 095

---
+++ Codigo_095/Codigo_095/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Codigo_095
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Maria";
+            string b = "Maria";
+            Console.WriteLine("Equals: " + a.Equals(b));
+
+            Console.WriteLine("GetHashCode Maria: " + a.GetHashCode());
+            Console.WriteLine("GetHashCode Alex/Maria: " + b.GetHashCode());
+        }
+    }
+}
