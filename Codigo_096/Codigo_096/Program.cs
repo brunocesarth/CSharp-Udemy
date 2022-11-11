29dacacd56f3deeb174b3c0e984bc979020e2276
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-11-10 21:16:49 -0300

    Codigo 096
    Equals e getHashCode

---
+++ Codigo_096/Codigo_096/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Codigo_096.Entities;
+
+namespace Codigo_096
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client() { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client() { Name = "Alex", Email = "alex@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b); //Compara o ponteiro de referência na memória
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
