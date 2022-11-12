30905750333fc3f257ee65aed5c2a65db13e0a7b
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-11-12 19:46:23 -0300

    Código 098 e 099

---
+++ Codigo_099/Codigo_099/Entities/Point.cs
@@ -0,0 +1,14 @@
+﻿namespace Codigo_099.Entities
+{
+    struct Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int x, int y) : this()
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
