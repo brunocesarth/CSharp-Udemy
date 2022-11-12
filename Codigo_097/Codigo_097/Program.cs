55d1f8ad4d9fc14ba46714f35e3802a8beb9f9d5
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-11-12 17:08:32 -0300

    Código_097
    HashSet e SortedSet

---
+++ Codigo_097/Codigo_097/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codigo_097
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine("Retorno do contains: " + set.Contains("Computer"));
+
+            foreach (string p in set)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
