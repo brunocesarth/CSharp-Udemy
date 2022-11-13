d426e84918c312d0ba84ae8aa9631860d2d2157c
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-11-12 21:41:19 -0300

    Código 101
    Exercício proposto

---
+++ Codigo_101/Codigo_101/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codigo_101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> CourseA = new HashSet<int>();
+            HashSet<int> CourseB = new HashSet<int>();
+            HashSet<int> CourseC = new HashSet<int>();
+
+            Console.Write("How many students for course A? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int matricula = int.Parse(Console.ReadLine());
+                CourseA.Add(matricula);
+            }
+
+            Console.Write("How many students for course B? ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int matricula = int.Parse(Console.ReadLine());
+                CourseB.Add(matricula);
+            }
+
+            Console.Write("How many students for course C? ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int matricula = int.Parse(Console.ReadLine());
+                CourseC.Add(matricula);
+            }
+
+            HashSet<int> all = new HashSet<int>(CourseA);
+            all.UnionWith(CourseB);
+            all.UnionWith(CourseC);
+            Console.WriteLine("Total students: " + all.Count);
+        }
+    }
+}
