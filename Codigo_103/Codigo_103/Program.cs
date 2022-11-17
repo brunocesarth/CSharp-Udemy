1af3bffb8243e063f436ec555e57d23319a84ffe
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-11-16 21:45:18 -0300

    Código 103
    Exercício dictionary

---
+++ Codigo_103/Codigo_103/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Codigo_103
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> values = new Dictionary<string, int>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using StreamReader sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(",");
+                    values.Add(line[0], int.Parse(line[1]));
+                    Console.WriteLine(values);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
