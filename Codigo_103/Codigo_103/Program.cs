15b973a959e7d3b6722d158212f18d5abb8d7876
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-11-21 20:12:53 -0300

    Update Program.cs

--- Codigo_103/Codigo_103/Program.cs
+++ Codigo_103/Codigo_103/Program.cs
@@ -8,7 +8,7 @@ namespace Codigo_103
     {
         static void Main(string[] args)
         {
-            Dictionary<string, int> values = new Dictionary<string, int>();
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
 
             Console.Write("Enter file full path: ");
             string path = Console.ReadLine();
@@ -19,8 +19,22 @@ namespace Codigo_103
                 while (!sr.EndOfStream)
                 {
                     string[] line = sr.ReadLine().Split(",");
-                    values.Add(line[0], int.Parse(line[1]));
-                    Console.WriteLine(values);
+                    string candidate = line[0];
+                    int votes = int.Parse(line[1]);
+
+                    if (dictionary.ContainsKey(candidate))
+                    {
+                        dictionary[candidate] += votes;
+                    }
+                    else
+                    {
+                        dictionary[candidate] = votes;
+                    }
+                }
+
+                foreach (var item in dictionary)
+                {
+                    Console.WriteLine(item.Key + ": " + item.Value);
                 }
             }
             catch (IOException e)
