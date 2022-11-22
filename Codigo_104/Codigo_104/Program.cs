c879d9bed27fa31585a1bcc1214e0d7f383c8cea
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-11-22 00:36:35 -0300

    Código 104
    Extension methods - Demo 1

---
+++ Codigo_104/Codigo_104/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Codigo_104
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2018, 11, 16, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
