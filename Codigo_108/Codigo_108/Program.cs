22dafe1b06478b8acf4c053d346c6e0b5848372b
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-11-23 20:25:25 -0300

    Código 108
    Delegates

---
+++ Codigo_108/Codigo_108/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Codigo_108.Services;
+
+namespace Codigo_108
+{
+    class Program
+    {
+        delegate double BinaryNumericOperation(double n1, double n2);
+
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+
+            double result = op(a, b);
+            //double result = op.Invoke(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
