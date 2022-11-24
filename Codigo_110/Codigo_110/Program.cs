ca8c8aa77f56977b7ca5bc042bbe2d7b2b84cf49
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-11-23 21:35:00 -0300

    Código 110
    Predicate RemoveAll

---
+++ Codigo_110/Codigo_110/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Codigo_110.Entities;
+
+namespace Codigo_110
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.00));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.RemoveAll(ProductTest);
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        public static bool ProductTest(Product p)
+        {
+            return p.Price >= 100.0;
+        }
+    }
+}
