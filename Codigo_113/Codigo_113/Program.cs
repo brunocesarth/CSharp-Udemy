d38949e137e2f5fc0cb440257c4fd18ba2d8981e
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-11-26 14:23:26 -0300

    Código 113
    Introdução ao LINQ

---
+++ Codigo_113/Codigo_113/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Codigo_113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Specify the data source
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+
+            // Define the query expression
+            // var result = numbers
+            IEnumerable<int> result = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10);
+
+            // Execute the query
+            foreach (int x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
