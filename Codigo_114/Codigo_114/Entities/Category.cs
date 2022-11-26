be9acd56bd63422e0ae3984b4da7407e2c1f5e10
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-11-26 20:27:10 -0300

    Código 114
    Demo LINQ

---
+++ Codigo_114/Codigo_114/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Codigo_114.Entities
+{
+    class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+    }
+}
