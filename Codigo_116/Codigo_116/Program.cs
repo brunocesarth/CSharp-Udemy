20e1a79c3d9fc6e7c263ef544f26e8191348ba4e
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-12-01 13:51:47 -0300

    Código 116

---
+++ Codigo_116/Codigo_116/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Codigo_116.Entities;
+
+namespace Codigo_116
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Product> list = new List<Product>();
+
+            using StreamReader sr = File.OpenText(path);
+            while (!sr.EndOfStream)
+            {
+                sr.ReadLine().Split(',');
+            }
+        }
+    }
+}
